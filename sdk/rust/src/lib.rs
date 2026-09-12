286720fb764a521dba1263b6718029cc8cd2023c
Alexander Mills <alex@oresoftware.com>
2026-09-11 21:32:10 -0500

    feat(DEN-390): export APM resource and latency primitives

--- sdk/rust/src/lib.rs
+++ sdk/rust/src/lib.rs
@@ -5,12 +5,19 @@ mod logger_core;
 
 pub use logger_core::*;
 
+pub mod apm;
 pub mod context;
 #[cfg(all(unix, feature = "launcher"))]
 pub mod launcher;
 pub mod shutdown;
 pub mod span;
 
+pub use apm::{
+    evaluate_resource_snapshot, measure, sample_filesystem, sample_process, sample_resources,
+    ApmError, FilesystemSnapshot, LatencyHistogram, LatencyHistogramConfig,
+    LatencyHistogramSnapshot, ProcessSnapshot, ResourceHealth, ResourcePressure,
+    ResourcePressureKind, ResourceSnapshot, ResourceThresholds, Timed,
+};
 pub use context::{
     apply_log_context, capture_log_context, contextualize_future, current_log_context,
     enter_log_context, merge_log_context, update_log_context, with_captured_log_context,
