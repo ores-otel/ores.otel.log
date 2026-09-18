ce8a18e897271b670d48b4830aaae59f6c0b88ae
Alexander Mills <alex@oresoftware.com>
2026-09-18 12:34:05 -0500

    [DEN-2843] export nearest config discovery and warning

--- sdk/rust/src/lib.rs
+++ sdk/rust/src/lib.rs
@@ -7,6 +7,7 @@ pub use logger_core::*;
 
 pub mod apm;
 pub mod config;
+pub mod config_discovery;
 pub mod context;
 pub mod http_shutdown;
 #[cfg(all(unix, feature = "launcher"))]
@@ -27,6 +28,11 @@ pub use config::{
     resolve_ores_otel_config, LoadOptions, LoadedOresOtelConfig, OresOtelConfigError,
     OresOtelFileConfig, OresOtelFileLayer, OresOtelRole, ResolveOptions, ResolvedMetrics,
     ResolvedOresOtelConfig, RuntimeRole, ORES_OTEL_CONFIG_BASENAME,
+};
+pub use config_discovery::{
+    discover_nearest_ores_otel_config, discover_nearest_ores_otel_config_from_cwd,
+    load_nearest_ores_otel_config, load_nearest_ores_otel_config_from_process_env,
+    warn_non_root_config, DiscoveredOresOtelConfig,
 };
 pub use context::{
     apply_log_context, capture_log_context, contextualize_future, current_log_context,
